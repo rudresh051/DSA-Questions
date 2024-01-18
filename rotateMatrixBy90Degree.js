1996a05743a8df6b1cb170f49dedf29e2a4e27a2
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-18 10:25:36 +0530

    rotate matrix 90 degree CW

--- rotateMatrixBy90Degree.js
+++ rotateMatrixBy90Degree.js
@@ -1,18 +1,17 @@
 function rotateBy90Clockwise(N, matrix){
-  //write code here
-  // console.log(N)
-  // console.log(matrix)
-  
   for(var layer=0;layer<Math.floor(N/2);layer++){
-      for(var i=layer;i<=N-1-layer;i++){
+      for(var i=layer;i<N-1-layer;i++){
           var temp = matrix[layer][i]
           matrix[layer][i] = matrix[N-1-i][layer]
-          matrix[N-1-i][layer] = matrix[N-1-i][N-1-i]
+          matrix[N-1-i][layer] = matrix[N-1-layer][N-1-i]
           matrix[N-1-layer][N-1-i] = matrix[i][N-1-layer]
           matrix[i][N-1-layer] = temp
       }
   }
-  
-  console.log(matrix)
-
+printMatrix(matrix);
+}
+function printMatrix(matrix){
+  for(var i=0;i<matrix.length;i++){
+      console.log(matrix[i].join(' '));
+  }
 }
