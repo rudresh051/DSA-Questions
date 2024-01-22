aedfba09ead330a20d2f1f5c7c67ae37b40118ff
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-22 15:29:43 +0530

    find first index of a key from left and right

---
+++ PythonQuestionsSolutions/findIndex.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findIndex (self,a, N, key ):
+        b = []
+        if key not in a:
+            return -1,-1
+        for i in range(N):
+            if a[i]==key:
+                b.append(i)
+        return min(b),max(b)
