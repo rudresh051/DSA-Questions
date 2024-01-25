8876f8c670182bae24af79fdf99bd67a78a617e3
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-25 18:09:17 +0530

    perfectArray

---
+++ PythonQuestionsSolutions/removeSpaces.py
@@ -0,0 +1,14 @@
+class Solution:
+    def modify(self, s):
+        # code here
+        y = s.strip()
+        # print(y)
+        arr = []
+        for x in y:
+            if x == " ":
+                continue
+            else:
+                arr.append(x)
+        
+        arr1 = "".join(arr)
+        return arr1
