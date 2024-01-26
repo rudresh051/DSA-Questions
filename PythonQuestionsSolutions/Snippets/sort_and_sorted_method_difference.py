24ec26d9d0e14967080c07625c97b848e46b1f3f
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-26 13:34:44 +0530

    difference between sort and sorted method

---
+++ PythonQuestionsSolutions/Snippets/sort_and_sorted_method_difference.py
@@ -0,0 +1,45 @@
+# 1. Sorting of Numbers in a list
+# L = [1,5,4,2,3]
+# print(sorted(L))
+# print(L)
+
+# L.sort()
+# print(L)
+
+
+# 2. Sorting of Strings given in a list ------------------
+# x_list = ['q','w','r','e','t','y']
+# sorted_list = sorted(x_list)
+# print(sorted_list)
+# print(x_list)
+
+# x_list.sort()
+# print(x_list)
+
+# 3. Sorting of Strings in a tuple ----------------------------
+# x_tuple = ('q','w','e','r','t','y')
+# print(sorted(x_tuple))
+# print(x_tuple)
+
+# print(x_tuple.sort()) # It will give error when trying to sort. Check it.
+# print(x_tuple)
+
+# 4. Reverse Order Sorting with Different Datatype -------------
+# Using sort() with reverse for List of Integers, Floating point numbers, and Strings
+# a. List of Integers
+# numbers = [1,3,4,2]
+# numbers.sort(reverse=True)
+# print(numbers)
+
+# b. List of Floating point Numbers
+# decimal_numbers = [2.01,2.00,3.67, 3.28,1.68]
+# print(sorted(decimal_numbers))
+# print(decimal_numbers)
+
+# print(decimal_numbers.sort(reverse=True))
+# print(decimal_numbers)
+
+# c. List of Strings
+# words = ["Geeks","For","Geeks"]
+# print(words.sort(reverse=True))
+# print(words)
