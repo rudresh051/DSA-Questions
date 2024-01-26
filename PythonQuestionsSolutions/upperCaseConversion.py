eacec483baf8e6150ef51b43e9a4c41df1210cf1
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-26 22:53:25 +0530

    convert first lettr of each word to uppercase in a string

---
+++ PythonQuestionsSolutions/upperCaseConversion.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+class Solution:
+    def transform(self, s):
+        # code here
+        res = s.title()
+        return res
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        ob = Solution()
+        print(ob.transform(s))
+# } Driver Code Ends
