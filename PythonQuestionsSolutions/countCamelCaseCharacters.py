6e7cbb662f9a5006acb76ce06b3639381f4ed4f8
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-26 23:16:54 +0530

    count number of camel case charcter in a string

---
+++ PythonQuestionsSolutions/countCamelCaseCharacters.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+    def countCamelCase (self,s):
+        # your code here
+        
+        count = 0
+        for i in range(len(s)):
+            if s[i] in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 
+            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
+                count = count+1
+            else:
+                pass
+        return count
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t = int (input ())
+for tc in range (t):
+    s = input ()
+    ob = Solution()
+    print (ob.countCamelCase (s))
+
+# Contributed By: Pranay Bansal
+
+# } Driver Code Ends
