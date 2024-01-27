8dbb961f5b6b7fe70d52d1e2f4ec3840387c61f7
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-27 15:43:02 +0530

    remove vowels from string

--- PythonQuestionsSolutions/findMedian.py
+++ PythonQuestionsSolutions/findMedian.py
@@ -1,23 +1,23 @@
 #User function Template for python3
-
+from math import floor
 class Solution:
-	def find_median(self, v):
-		# Code here
-		print(v)
-		v_sorted = sorted(v)
-		print(v_sorted)
-		size = len(v_sorted)
-		print(size)
-		
-		if size%2==1:
-		    medianIndex = (size+1)/2
-		    value = v_sorted[medianIndex]
-		    return floor(value)
-	    elif size%2==0:
-	        medianIndex1 = (size-1)/2
-	        medianIndex2 = (size+1)/2
-	        value = (v_sorted[medianIndex1]+v_sorted[medianIndex2])/2
-	        return floor(value)
+    def find_median(self, v):
+        # Code here
+        print(v)
+        v_sorted = sorted(v)
+        print(v_sorted)
+        size = len(v_sorted)
+        print(size)
+
+        if size%2==1:
+            medianIndex = (size+1)/2
+            value = v_sorted[medianIndex]
+            return floor(value)
+        elif size%2==0:
+            medianIndex1 = (size-1)/2
+            medianIndex2 = (size+1)/2
+            value = (v_sorted[medianIndex1]+v_sorted[medianIndex2])/2
+            return floor(value)
 
 
 #{ 
@@ -25,11 +25,11 @@ class Solution:
 #Initial Template for Python 3
 
 if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		n = int(input())
-		v = list(map(int,input().split())) 
-		ob = Solution();
-		ans = ob.find_median(v)
-		print(ans)
+    T=int(input())
+    for i in range(T):
+        n = int(input())
+        v = list(map(int,input().split()))
+        ob = Solution();
+        ans = ob.find_median(v)
+        print(ans)
 # } Driver Code Ends
---
+++ PythonQuestionsSolutions/removeVowelsFromString.py
@@ -0,0 +1,42 @@
+# User function Template for python3
+class Solution:
+    def removeVowels(self, S):
+        # code here
+        res = []
+        res_without_vowel = []
+        for i in range(len(S)):
+            res.append(S[i])
+        # print("res",res)
+
+        for i in range(len(res)):
+            if (
+                res[i] == "a"
+                or res[i] == "e"
+                or res[i] == "i"
+                or res[i] == "o"
+                or res[i] == "u"
+            ):
+                continue
+            else:
+                res_without_vowel.append(res[i])
+        # print("res_without_vowel",res_without_vowel)
+        s1 = "".join(res_without_vowel)
+        return s1
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    T = int(input())
+    for i in range(T):
+        s = input()
+
+        ob = Solution()
+        answer = ob.removeVowels(s)
+
+        print(answer)
+
+
+# } Driver Code Ends
