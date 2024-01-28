1d01619f4ba21651419df6b8f8005653ce7df7ee
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-28 13:29:50 +0530

    mean at every point

---
+++ PythonQuestionsSolutions/averageInStream.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+class Solution:
+    def streamAvg(self, arr, n):
+        # code here
+        res = []
+        count = 1
+        sum = 0
+        for i in range(len(arr)):
+            sum = sum+arr[i]
+            mean = sum/count
+            res.append(mean)
+            count = count+1
+
+      	return res
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.streamAvg(arr, n)
+        for x in ans:
+            print('%.2f'%x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
