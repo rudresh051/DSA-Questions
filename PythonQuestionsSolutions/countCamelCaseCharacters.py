610b0f5778ba43ef6ac6f77b16be976e4c9d4c37
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-29 00:02:39 +0530

    optimized

--- PythonQuestionsSolutions/countCamelCaseCharacters.py
+++ PythonQuestionsSolutions/countCamelCaseCharacters.py
@@ -4,14 +4,25 @@ class Solution:
     def countCamelCase (self,s):
         # your code here
         
+        # count = 0
+        # for i in range(len(s)):
+        #     if s[i] in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 
+        #     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
+        #         count = count+1
+        #     else:
+        #         pass
+        # return count
+        
+        # Method2 - Optimized
+        set1 = set('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        # print(set(s))
         count = 0
         for i in range(len(s)):
-            if s[i] in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 
-            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
-                count = count+1
+            if s[i] in set1:
+                count=count+1
             else:
-                pass
-        return count
+                continue
+        return (count)
 
 
 
