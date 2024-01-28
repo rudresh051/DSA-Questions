6966c36206339d62f259ca6682314be0bac9b77d
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-28 11:56:19 +0530

    print the left element optimized

--- PythonQuestionsSolutions/printTheLeftElement.py
+++ PythonQuestionsSolutions/printTheLeftElement.py
@@ -1,36 +1,41 @@
-#User function Template for python3
+# User function Template for python3
+import math
+
 
 class Solution:
     def leftElement(self, arr, n):
-    # Your code goes here 
+        # Your code goes here
         arr_sorted = sorted(arr)
         # print(arr_sorted)
-        for i in range(len(arr_sorted)-1):
-            if i%2==0:
-                arr_sorted.pop()
-            else:
-                arr_sorted.pop(0)
-        
-        s1 = "".join(map(str,arr_sorted))
-        return s1
-        
-            
-                
+        # for i in range(len(arr_sorted)-1):
+        #     if i%2==0:
+        #         arr_sorted.pop()
+        #     else:
+        #         arr_sorted.pop(0)
+
+        # s1 = "".join(map(str,arr_sorted))
+        # return s1
+
+        # Method 2 - Optimized
+        size = (len(arr_sorted))
+
+        if size % 2 == 0:
+            return arr_sorted[(size // 2) - 1]
+        else:
+            return arr_sorted[math.floor(size / 2)]
 
 
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
+# {
+# Driver Code Starts
+# Initial Template for Python 3
 
 def main():
-
     T = int(input())
 
-    while(T > 0):
+    while (T > 0):
         n = int(input())
         a = [int(x) for x in input().strip().split()]
-        ob=Solution()
+        ob = Solution()
         print(ob.leftElement(a, n))
 
         T -= 1
@@ -39,9 +44,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-
-
-    
 # } Driver Code Ends
