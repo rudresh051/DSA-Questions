ffeaafc3a0ce9bd16fde47435ab4019680defbc6
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-29 13:59:37 +0530

    remove characters except numbers

---
+++ PythonQuestionsSolutions/removeCharactersFromAlphanumericString.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+class Solution:
+    def removeCharacters(self, S):
+        # code here
+        res = []
+        num = [1,2,3,4,5,6,7,8,9,0]
+        num_str = ''.join(map(str, num))  # Convert each digit to string and concatenate
+
+
+        for i in range(len(S)):
+          #  print("type S[i]",type(S[i]))
+            if S[i] in num_str:
+                res.append(S[i])
+            else:
+                continue
+
+       # return res
+        return "".join(res)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        s = input()
+
+        ob = Solution()
+        answer = ob.removeCharacters(s)
+
+        print(answer)
+
+
+# } Driver Code Ends
