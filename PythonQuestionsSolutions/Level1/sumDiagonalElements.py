4119369a786f95544b7824fe1e781e9f8cf2fe18
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-01-30 14:41:09 +0530

    sum the diagonal elements

--- PythonQuestionsSolutions/Level1/sumDiagonalElements.py
+++ PythonQuestionsSolutions/Level1/sumDiagonalElements.py
@@ -1,35 +1,27 @@
-# User function Template for python3
+#User function Template for python3
 
 class Solution:
     def DiagonalSum(self, matrix):
         # Code here
-        row = n
+        sum = 0
+        for i in range(len(matrix)):
+            sum = sum + matrix[i][i] + matrix[i][len(matrix)-1-i]
+        
 
-    # 		print("row",row)
-    # 		print("matlen",len(matrix))
-    col = n
-    sum = 0
-    for row in range(len(matrix)):
-        print("row", row)
-        for val in row:
-            print("val", val)
-            if row == col:
-                sum = sum + val
-
-    return sum
+	return sum
 
 
-# {
-# Driver Code Starts
-# Initial Template for Python 3
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
 
 if __name__ == '__main__':
-    T = int(input())
+    T=int(input())
     for i in range(T):
         n = int(input())
         matrix = []
         for _ in range(n):
-            matrix.append(list(map(int, input().split())))
+            matrix.append(list(map(int,input().split())))
         ob = Solution()
         ans = ob.DiagonalSum(matrix)
         print(ans)
