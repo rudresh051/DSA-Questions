908a56107e967d0a2f3a3107148c35f891eab622
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-02-01 16:44:31 +0530

    reverse the number

--- PythonQuestionsSolutions/Level1/reverseTheNumber.py
+++ PythonQuestionsSolutions/Level1/reverseTheNumber.py
@@ -1,18 +1,18 @@
 #User function Template for python3
 
 class Solution:
-	def reverse_digit(self, n):
-		# Code here
-		str1 = str(n)
+    def reverse_digit(self, n):
+        # Code here
+        str1 = str(n)
 # 		print("str1",str1)
-		list1 = list(str1)
+        list1 = list(str1)
 # 		print("list1",list1)
-		rev = []
-		for i in range(len(list1)-1,-1,-1):
-		    rev.append(list1[i])
-	   # print("rev",rev)
-	    
-	    for i in range(len(rev)):
+        rev = []
+        for i in range(len(list1)-1,-1,-1):
+            rev.append(list1[i])
+       # print("rev",rev)
+
+        for i in range(len(rev)):
             if rev[i]=='0':
                 continue
             elif rev[i]!='0':
@@ -24,13 +24,13 @@ class Solution:
         result = rev[index:len(rev)]
         # print(rev[index:len(rev)-1])
         return "".join(result)
-	    
-	   
-	            
-	            
-		    
-	
-		
+
+
+
+
+
+
+
 
 
 #{ 
@@ -38,10 +38,10 @@ class Solution:
 #Initial Template for Python 3
 
 if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		n = int(input())
-		ob = Solution();
-		ans = ob.reverse_digit(n)
-		print(ans)
+    T=int(input())
+    for i in range(T):
+        n = int(input())
+        ob = Solution();
+        ans = ob.reverse_digit(n)
+        print(ans)
 # } Driver Code Ends
