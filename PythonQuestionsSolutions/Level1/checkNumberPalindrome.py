2c70ac9305f040729f7e5e2decec4ec4107242f1
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-02-01 11:37:51 +0530

    find median of an array

--- PythonQuestionsSolutions/Level1/checkNumberPalindrome.py
+++ PythonQuestionsSolutions/Level1/checkNumberPalindrome.py
@@ -7,24 +7,24 @@ class Solution:
         s1 = str(n)
         s2 = []
 
-    # print(type(s1))
+        # print(type(s1))
 
-    for i in range(len(s1) - 1, -1, -1):
-        s2.append(s1[i])
+        for i in range(len(s1) - 1, -1, -1):
+            s2.append(s1[i])
 
-    # print("s2",s2)
+        # print("s2",s2)
 
-    s3 = "".join(map(str, s2))
-    # print("s3",s3)
-    # print(type(s3))
+        s3 = "".join(map(str, s2))
+        # print("s3",s3)
+        # print(type(s3))
 
-    n1 = int(s3)
-    # print(type(n1))
+        n1 = int(s3)
+        # print(type(n1))
 
-    if n == n1:
-        return "Yes"
-    else:
-        return "No"
+        if n == n1:
+            return "Yes"
+        else:
+            return "No"
 
 
 # {
--- PythonQuestionsSolutions/Level1/findMedian.py
+++ PythonQuestionsSolutions/Level1/findMedian.py
@@ -3,19 +3,20 @@ from math import floor
 class Solution:
     def find_median(self, v):
         # Code here
-        print(v)
+# 		print(v)
         v_sorted = sorted(v)
-        print(v_sorted)
+# 		print(v_sorted)
         size = len(v_sorted)
-        print(size)
+# 		print(size)
 
         if size%2==1:
-            medianIndex = (size+1)/2
+            medianIndex = floor((size)//2)
+          #  print("medianIndex",medianIndex)
             value = v_sorted[medianIndex]
-            return floor(value)
+            return (value)
         elif size%2==0:
-            medianIndex1 = (size-1)/2
-            medianIndex2 = (size+1)/2
+            medianIndex1 = (size-1)//2
+            medianIndex2 = (size+1)//2
             value = (v_sorted[medianIndex1]+v_sorted[medianIndex2])/2
             return floor(value)
 
