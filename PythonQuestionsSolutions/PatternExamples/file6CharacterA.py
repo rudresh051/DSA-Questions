695e7b581246c081b32beae98644fdbb3177b2a9
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-02-05 14:40:35 +0530

    Character B pattern

--- PythonQuestionsSolutions/PatternExamples/file6CharacterA.py
+++ PythonQuestionsSolutions/PatternExamples/file6CharacterA.py
@@ -1,8 +1,9 @@
-
 for row in range(7):
-  for col in range(5):
-    if ((col==0 or col==4) and row!=0) or ((row==0 or row==3) and (col>0 and col<4)):
-      print("*", end="")
-    else:
-      print(end=" ") # This print is for space between *
-  print() # This is for new line after printing * in one row
+    for col in range(5):
+        if ((col == 0 or col == 4) and row != 0) or (
+            (row == 0 or row == 3) and (col > 0 and col < 4)
+        ):
+            print("*", end="")
+        else:
+            print(end=" ")  # This print is for space between *
+    print()  # This is for new line after printing * in one row
