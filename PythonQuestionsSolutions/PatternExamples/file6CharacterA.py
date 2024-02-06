b337893061078cda9cf4e045c011df9a10416f65
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-02-06 10:20:08 +0530

    Added comments

--- PythonQuestionsSolutions/PatternExamples/file6CharacterA.py
+++ PythonQuestionsSolutions/PatternExamples/file6CharacterA.py
@@ -1,3 +1,11 @@
+#  *** 
+# *   *
+# *   *
+# *****
+# *   *
+# *   *
+# *   *
+
 for row in range(7):
     for col in range(5):
         if ((col == 0 or col == 4) and row != 0) or (
--- PythonQuestionsSolutions/PatternExamples/file8CharacterC.py
+++ PythonQuestionsSolutions/PatternExamples/file8CharacterC.py
@@ -1,3 +1,10 @@
+#  ****
+# *
+# *
+# *
+# *
+# *
+#  ****
 for row in range(7):
   for col in range(5):
     if ((col==0 and (row!=0 and row!=6)) or ((row==0 or row==6) and col>0)):
