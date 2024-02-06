804ce9d26910b6829f66ee0af6bd563a65c22d1a
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-02-06 12:01:49 +0530

    Pattern H and I

--- PythonQuestionsSolutions/PatternExamples/file13CharacterH.py
+++ PythonQuestionsSolutions/PatternExamples/file13CharacterH.py
@@ -1,14 +1,13 @@
+# H   H
+# H   H
+# H   H
 # HHHHH
-#   H
-#   H
-#   H
-#   H
-#   H
-# HHHHH
-
+# H   H
+# H   H
+# H   H
 for row in range(7):
   for col in range(5):
-    if (col==2) or ((row==0 or row==6) and (col==0 or col==1 or col==3 or col==4)):
+    if (col==0) or (col==4) or (row==3 and (col>0 and col<4)):
       print("H", end="")
     else:
       print(end=" ")
