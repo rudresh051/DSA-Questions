e5ddcdb7c955d4695f2fb132e44b19729014ce52
Rudreshwar Jha <56826499+rudresh051@users.noreply.github.com>
2024-02-21 19:40:01 +0530

    count words in a sentence using java

---
+++ JavaQuestionsSolutions/Programs/countWordsInSentence.java
@@ -0,0 +1,12 @@
+// count words
+public class Main
+{
+	public static void main(String[] args) {
+		String words = "One Two Three Four";
+		int countWords = words.split("\\s").length;
+		System.out.println(countWords);
+	}
+}
+
+// Output 
+// 4 
