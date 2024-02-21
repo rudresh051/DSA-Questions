3701b7a9a3af95159622c995838a9e326491170f
Rudreshwar Jha <56826499+rudresh051@users.noreply.github.com>
2024-02-21 20:21:09 +0530

    String method snippet added

---
+++ JavaQuestionsSolutions/Snippets/stringMethods.java
@@ -0,0 +1,21 @@
+public class Main{
+    public static void main(String[] args){
+        // String = a reference data type that can store one or more characters reference data types have
+        // access to useful methods
+
+        String name = "Hello world";
+        // boolean result = name.equals("Hello world"); // True
+        // int result = name.length(); // 11
+        // char result = name.charAt(0); // H
+        // int result = name.indexOf("o"); // 4
+        // String result = name.toUpperCase(); // "HELLO WORLD"
+        // String result = name.toLowerCase(); // "hello world"
+        // String result = name.trim(); // trim method will remove the extra empty spaces
+        // String result = name.replace("o", "y") // Replace method will all the o char with y char
+        System.out.println(result);
+
+        String name1 = "";
+        boolean result1 = name.isEmpty();
+        System.out.println(result1);
+    }
+}
