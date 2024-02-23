f848ee7713235889f78b2768b64a2c79ab1fe83f
Rudreshwar Jha <56826499+rudresh051@users.noreply.github.com>
2024-02-23 11:57:18 +0530

    area of rectangle using java

---
+++ JavaQuestionsSolutions/Programs/areaOfRectangle.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main (String[] args) {
+        int length = 5;
+        int breadth = 9;
+        
+        int area = length*breadth;
+        System.out.println(area);;
+    }
+}
