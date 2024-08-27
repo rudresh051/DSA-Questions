898b0762b017019b0531ce93f6bffe6a28a848d0
Rudreshwar Jha <56826499+rudresh051@users.noreply.github.com>
2024-08-27 23:26:57 +0530

    roman to integer

---
+++ JavaScriptQuestionsSolutions/romanToInteger.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/roman-to-integer/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var I = 1
+    var V = 5
+    var X = 10
+    var L = 50
+    var C = 100
+    var D = 500
+    var M = 1000
+    var str1 = s.split(" ")
+    console.log(s)
+};
