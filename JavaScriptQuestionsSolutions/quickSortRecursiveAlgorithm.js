a61ef9711df9bd5a373d1e5d7d85fc7ee44b0983
Rudreshwar Jha <56826499+rudresh051@users.noreply.github.com>
2024-09-13 23:30:06 +0530

    DSA Problem check latest version

--- JavaScriptQuestionsSolutions/quickSortRecursiveAlgorithm.js
+++ JavaScriptQuestionsSolutions/quickSortRecursiveAlgorithm.js
@@ -1,40 +1,28 @@
 function runProgram(input) {
-  // Write code here
-//   console.log(input)
-  input = input.split("\n")
-//   console.log(input)
-  var arr = input[1].split(" ").map(function(val){
-      return parseInt(val)
+  input1  = input.split("\n")
+//   console.log("input[0]",input1[0])
+//   console.log("input[1]",input1[1])
+  var arr1 = input1[0].split(".").map(function(value){
+      return parseInt(value)
   })
-//   console.log(arr)
-  var n = arr.length
+//   console.log("arr1",arr1)
+  var num1 = arr1.join("")
+//   console.log("num1",num1)
+//   console.log(typeof(num1))
   
-  function quickSort(arr,low,high){
-      if(low<high){
-          var pivotIndex = partition(arr,low,high)
-          quickSort(arr,low,pivotIndex-1)
-          quickSort(arr,pivotIndex+1,high)
-      }
+  var arr2 = input1[1].split(".").map(function(value){
+      return parseInt(value)
+  })
+//   console.log("arr2",arr2)
+  var num2 = arr2.join("")
+//   console.log("num2",num2)
+//   console.log(typeof(num2))
+  if(parseInt(num1)<parseInt(num2)){
+      console.log("Version 1 is the latest.")
+  }else{
+      console.log("Version 2 is the latest.")
   }
-  function partition(arr,low,high){
-      var pivot = arr[high]
-      var i=low-1
-      for(var j=low;j<high;j++){
-          if(arr[j]<pivot){
-              i++
-              swap(arr,i,j)
-          }
-      }
-      swap(arr,i+1,high)
-      return i+1
-  }
-  function swap(arr,i,j){
-      var temp = arr[i]
-      arr[i]=arr[j]
-      arr[j]=temp
-  }
-  quickSort(arr,0,arr.length-1)
-  console.log(arr.join(" "))
+  
 }
 
 if (process.env.USER === "") {
